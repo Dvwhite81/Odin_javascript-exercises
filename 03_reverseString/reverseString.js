86ecdd172189c9955826b8e5e3938e06fa836200
Dvwhite81 <Dvwhite1981@gmail.com>
2023-09-13 16:55:14 -0400

    1-5 Complete

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+const reverseString = function(str) {
 
+    let result = '';
+    let i = str.length - 1;
+
+    while (i >= 0) {
+        result += str[i];
+        i--;
+    }
+
+    return result;
 };
 
 // Do not edit below this line
